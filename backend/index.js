df8c4d3923dd799a962725555690d12f9dc1c87a
IanNotermansPXL <ian.notermans@student.pxl.be>
2024-05-08 16:57:20 +0200

    Fixed tot en met FOUT #3

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,12 @@ const pool = new pg.Pool({
 
 console.log("Connecting...:")
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -30,14 +35,20 @@ app.get('/authenticate/:username/:password', async (request, response) => {
     const username = request.params.username;
     const password = request.params.password;
 
-    const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
-    console.log(query);
-    pool.query(query, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)});
-      
+    const query = 'SELECT * FROM users WHERE user_name=$1 AND password = crypt($2, password)';
+    const values = [username, password];
+
+    try {
+        const results = await pool.query(query, values);
+        if (results.rows.length > 0) {
+            response.status(200).json({ message: 'Authentication successful.' });
+        } else {
+            response.status(401).json({ error: 'Authentication failed.' });
+        }
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: 'An error occurred while processing your request.' });
+    }
 });
 
 app.listen(port, () => {
